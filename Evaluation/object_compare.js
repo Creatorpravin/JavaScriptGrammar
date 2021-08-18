b33f9723bd2f435d7f2125f56e2d744130ce17d5
YuvarajMoorthi <moorthiyuvaraj10@gmail.com>
2021-08-18 13:33:28 +0530

    Evaluation1

---
+++ Evaluation/object_compare.js
@@ -0,0 +1,17 @@
+var a={id: 1, name: "edison"},b={id: 1, name: "edison"};
+var c={id: 1, name: "edison"}, d={id: 2, name: "edison"};
+console.log(objcmp(a,b));     //true
+console.log(objcmp(c,d));     //false
+
+function objcmp(x,y){
+    let A=Object.getOwnPropertyNames(x);
+    let B=Object.getOwnPropertyNames(y);
+    if(A.length!=B.length)
+      return false;
+    for(let i=0;i<A.length;i++){
+         let propName=A[i];
+         if(x[propName]!==y[propName])
+           return false;
+     } 
+     return true;
+}
